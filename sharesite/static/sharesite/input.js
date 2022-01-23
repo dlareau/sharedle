d2f4b1c38b0bbf0750b1f395a503b3d0bf056cbc
Dillon Lareau <flybye22@gmail.com>
2022-01-23 11:28:15 -0500

    mobile input

--- sharesite/static/sharesite/input.js
+++ sharesite/static/sharesite/input.js
@@ -32,11 +32,13 @@ function decodeWord(result) {
 }
 
 function populateGrid(grid_str) {
-  for (var i = 0; i < grid.length; i++) {
-    $("#input-box-" + (i + 1)).text(grid[i]);
-    if(grid[i] == correctWord[(i) % 5]) {
+  grid_str = grid_str.toUpperCase();
+  for (var i = 0; i < grid_str.length; i++) {
+    $("#input-box-" + (i + 1)).text(grid_str[i]);
+    $("#input-box-" + (i + 1)).data("val", grid_str[i]);
+    if(grid_str[i] == correctWord[(i) % 5]) {
       $("#input-box-" + (i + 1)).addClass("correct");
-    } else if (correctWord.indexOf(grid[i]) !== -1) {
+    } else if (correctWord.indexOf(grid_str[i]) !== -1) {
       $("#input-box-" + (i + 1)).addClass("semi-correct");
     } else {
       $("#input-box-" + (i + 1)).addClass("incorrect");
@@ -81,9 +83,69 @@ function handlePaste(paste) {
   }
 }
 
+function clearGrid() {
+  for (var i = 0; i < 30; i++) {
+    $("#input-box-" + (i + 1)).text("");
+    $("#input-box-" + (i + 1)).data("val", "");
+    $("#input-box-" + (i + 1)).removeClass("correct");
+    $("#input-box-" + (i + 1)).removeClass("semi-correct");
+    $("#input-box-" + (i + 1)).removeClass("incorrect");
+    $("#input-box-" + (i + 1)).addClass("empty");
+  }
+}
+
+function gridToTextbox() {
+  $("#dummy-box").val(getGridString());
+}
+
+function textboxToGrid() {
+  $("#dummy-box").val($("#dummy-box").val().toUpperCase().replace(/\s/g, ''))
+  clearGrid();
+  populateGrid($("#dummy-box").val());
+  $(".current").removeClass("current");
+  index = $("#dummy-box")[0].selectionStart;
+  $("#input-box-" + index).addClass("current");
+
+}
+
+function addEventToGrid(keyEvent) {
+  $(".current").removeClass("current");
+  if(event.key.toUpperCase() == "BACKSPACE") {
+    if(index > 1){
+      index--;
+    }
+    $("#input-box-" + index).text("");
+    $("#input-box-" + index).data("val", "");
+    $("#input-box-" + index).removeClass("correct");
+    $("#input-box-" + index).removeClass("semi-correct");
+    $("#input-box-" + index).removeClass("incorrect");
+    $("#input-box-" + index).addClass("empty");
+  }
+  if(event.key.match(/^[a-z]$/i) && !event.ctrlKey && !event.metaKey && !event.altKey) {
+    $("#input-box-" + index).text(event.key.toUpperCase());
+    $("#input-box-" + index).data("val", event.key.toUpperCase());
+    $("#input-box-" + index).removeClass("empty");
+    $("#input-box-" + index).removeClass("correct");
+    $("#input-box-" + index).removeClass("semi-correct");
+    $("#input-box-" + index).removeClass("incorrect");
+    if(event.key.toUpperCase() == correctWord[(index - 1) % 5]) {
+      $("#input-box-" + index).addClass("correct");
+    } else if (correctWord.indexOf(event.key.toUpperCase()) !== -1) {
+      $("#input-box-" + index).addClass("semi-correct");
+    } else {
+      $("#input-box-" + index).addClass("incorrect");
+    }
+    if(index <= 30){
+      index++;
+    }
+  }
+  $("#input-box-" + index).addClass("current");
+  gridToTextbox();
+}
+
 $('document').ready(function(){
 
-  $("#wordle-data").submit( function(e) {
+  $("#form-submit").click( function(e) {
     if(grid == "") {
       data = getGridString();
       if(data == "" || data.length % 5 != 0){
@@ -93,55 +155,28 @@ $('document').ready(function(){
       $("<input />").attr("type", "hidden")
           .attr("name", "grid")
           .attr("value", data)
-          .appendTo($(this));
-      return true;
+          .appendTo($("#wordle-data"));
     } else {
       $("<input />").attr("type", "hidden")
           .attr("name", "operation")
           .attr("value", "clear")
-          .appendTo($(this));
+          .appendTo($("#wordle-data"));
     }
+    $("#wordle-data").submit()
   });
 
   if(grid != "") {
     populateGrid(grid);
   } else {
-    document.addEventListener('keyup', function(event) {
-      $("#dummy-box").val("");
+    document.addEventListener('keydown', function(event) {
+      if(!$("#dummy-box").is(":focus")) {
+        addEventToGrid(event);
+      }
     });
 
-    document.addEventListener('keydown', function(event) {
-      $("#dummy-box").val("");
+    $("#dummy-box").click(function() {
       $(".current").removeClass("current");
-      if(event.key.toUpperCase() == "BACKSPACE") {
-        if(index > 1){
-          index--;
-        }
-        $("#input-box-" + index).text("");
-        $("#input-box-" + index).data("val", "");
-        $("#input-box-" + index).removeClass("correct");
-        $("#input-box-" + index).removeClass("semi-correct");
-        $("#input-box-" + index).removeClass("incorrect");
-        $("#input-box-" + index).addClass("empty");
-      }
-      if(event.key.match(/^[a-z]$/i) && !event.ctrlKey && !event.metaKey && !event.altKey) {
-        $("#input-box-" + index).text(event.key.toUpperCase());
-        $("#input-box-" + index).data("val", event.key.toUpperCase());
-        $("#input-box-" + index).removeClass("empty");
-        $("#input-box-" + index).removeClass("correct");
-        $("#input-box-" + index).removeClass("semi-correct");
-        $("#input-box-" + index).removeClass("incorrect");
-        if(event.key.toUpperCase() == correctWord[(index - 1) % 5]) {
-          $("#input-box-" + index).addClass("correct");
-        } else if (correctWord.indexOf(event.key.toUpperCase()) !== -1) {
-          $("#input-box-" + index).addClass("semi-correct");
-        } else {
-          $("#input-box-" + index).addClass("incorrect");
-        }
-        if(index <= 30){
-          index++;
-        }
-      }
+      index = $("#dummy-box")[0].selectionStart;
       $("#input-box-" + index).addClass("current");
     });
 
@@ -149,9 +184,12 @@ $('document').ready(function(){
       $(".current").removeClass("current");
       $(this).addClass("current");
       index = $(this).data("tile");
+      $("#dummy-box").focus();
+      $("#dummy-box")[0].setSelectionRange(index, index);
     });
 
     document.addEventListener('paste', (event) => {
+      $("#test").text("paste");
       let paste = (event.clipboardData || window.clipboardData).getData('text');
       handlePaste(paste);
       event.preventDefault();
